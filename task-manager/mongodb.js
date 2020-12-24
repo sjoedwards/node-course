f73f7dfca0ad8ce5006f019603bc6cb7c9d89089
sjoedwards <samuel.j.o.edwards@gmail.com>
2020-12-24 22:41:55 +0000

    Async test

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -3,64 +3,72 @@ const { MongoClient } = require("mongodb");
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
 
+const collectionExists = (collection) =>
+  collection
+    .find({})
+    .toArray()
+    .then((result) => result.length > 0);
+
+const dropCollectionIfExists = (collection) =>
+  collectionExists(collection)
+    .then((exists) => {
+      if (exists) {
+        return collection.drop();
+      } else {
+        return;
+      }
+    })
+    .catch((e) => {
+      throw new Error(e);
+    });
+
+let users;
+let tasks;
 MongoClient.connect(connectionURL, { useNewUrlParser: true })
   .then((client) => {
     const db = client.db(databaseName);
-    const users = db.collection("users");
-    const collectionExists = (collection) =>
-      collection
-        .find({})
-        .toArray()
-        .then((result) => result.length > 0);
-
-    const dropCollectionIfExists = (collection) =>
-      new Promise((resolve) => {
-        collectionExists(collection).then((exists) => {
-          if (exists) {
-            collection.drop().then(() => {
-              resolve();
-            });
-          } else {
-            resolve();
-          }
-        });
-      });
-
-    dropCollectionIfExists(users).then(
-      users
-        .insertOne({ name: "Jim", age: 25 })
-        .then(({ ops }) => {
-          const { _id } = ops[0];
-          users
-            .updateOne({ _id }, { $set: { name: "Jimbo" } })
-            .then(() => {
-              console.log("Success!");
-            })
-            .catch((e) => console.log(`Couldn't update user: ${e}`));
-        })
-        .catch(() => console.log("Couldn't insert user"))
-    );
-
-    const tasks = db.collection("tasks");
-
-    dropCollectionIfExists(tasks).then(() =>
-      tasks
-        .insertMany([
-          { description: "Clean the house", completed: true },
-          { description: "Renew inpection", completed: false },
-          { description: "Pot plants", completed: false },
-        ])
-        .then(() => {
-          tasks
-            .updateMany({ completed: false }, { $set: { completed: true } })
-            .then(() => {
-              tasks.findOne({}).then(({ description }) => {
-                tasks.deleteOne({ description: "Clean the house" });
-              });
-            })
-            .catch((e) => console.log("Couldn't update tasks", e));
-        })
-        .catch((e) => console.log("Couldn't insert tasks", e))
-    );
+    users = db.collection("users");
+    tasks = db.collection("tasks");
+    return;
   })
-  .catch(() => console.log("Unable to connect to database"));
+  .then(() => dropCollectionIfExists(users))
+  .then(() => users.insertOne({ name: "Jim", age: 25 }))
+  .then(({ ops }) => {
+    const { _id } = ops[0];
+    return users.updateOne({ _id }, { $set: { name: "Jimbo" } });
+  })
+  .then(() => {
+    return console.log("Success!");
+  })
+  .catch((e) => console.log(`Couldn't update user: ${e}`))
+  .then(() => dropCollectionIfExists(tasks))
+  .then(() =>
+    tasks.insertMany([
+      { description: "Clean the house", completed: true },
+      { description: "Renew inpection", completed: false },
+      { description: "Pot plants", completed: false },
+    ])
+  )
+  .then(() =>
+    tasks.updateMany({ completed: false }, { $set: { completed: true } })
+  )
+  .then(() => tasks.findOne({}))
+  .then(({ description }) => tasks.deleteOne({ description }))
+  .catch((e) => console.log("Couldn't update tasks", e));
+
+(async () => {
+  try {
+    const client = await MongoClient.connect(connectionURL, {
+      useNewUrlParser: true,
+    });
+    const db = client.db(databaseName);
+    const users = db.collection("usersAsync");
+    await dropCollectionIfExists(users);
+    const { ops } = await users.insertOne({ name: "Jim", age: 25 });
+    const { _id } = ops[0];
+    await users.updateOne({ _id }, { $set: { name: "JimboAsync" } });
+    console.log("Success");
+  } catch (e) {
+    throw new Error(`Unable to update users ${e}`);
+  }
+})();
