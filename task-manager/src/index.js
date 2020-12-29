6a17e3d69279f88e7d565663fda5e48e7e05abaf
sjoedwards <samuel.j.o.edwards@gmail.com>
2020-12-29 15:53:00 +0000

    Added correct casing for env var

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
