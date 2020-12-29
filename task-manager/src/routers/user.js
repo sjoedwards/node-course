b09a73c11c2cdbae70cf013d52f3361496d48434
sjoedwards <samuel.j.o.edwards@gmail.com>
2020-12-29 13:10:08 +0000

    Avatar

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -3,6 +3,8 @@ const User = require("../models/user");
 const router = new express.Router();
 const handleError = require("../util/handleError");
 const auth = require("../middleware/auth");
+const multer = require("multer");
+const sharp = require("sharp");
 
 router.get("/users/me", auth, async (req, res) => {
   // Auth middleware adds user to req object
@@ -110,6 +112,84 @@ router.delete("/users/me", auth, async (req, res) => {
     return res.send(req.user);
   } catch (e) {
     return handleError(res, e, 400, "Error deleting user");
+  }
+});
+
+// File uploading uses the multer library
+const upload = multer({
+  // Below dest means that the file will be saved to the filesystem - we don't want to do this
+  // dest: "avatars",
+  limits: {
+    // Must be less than 1mb
+    fileSize: 1 * 1024 * 1024,
+  },
+  fileFilter(req, file, callback) {
+    // Must be an image
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return callback(new Error("Please upload an image"));
+    }
+
+    callback(undefined, true);
+  },
+});
+
+router.post(
+  "/users/me/avatar",
+  [auth, upload.single("avatar")],
+  async (req, res) => {
+    //Sharp returns promises, so await is required
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
+    req.user.avatar = buffer;
+    await req.user.save();
+    return res.send();
+  },
+  // Needs to have this exact set of arguments, even if unused, to let express know its middleware - its a bit silly
+  // eslint-disable-next-line no-unused-vars
+  (err, req, res, next) => {
+    res.status(400).send({ error: err.message });
+  }
+);
+
+router.delete("/users/me/avatar", auth, async (req, res) => {
+  try {
+    req.user.avatar = undefined;
+    await req.user.save();
+    return res.send();
+  } catch (e) {
+    return handleError(res, e, 400, "Error deleting avatar");
+  }
+});
+
+router.get("/users/me/avatar", auth, async (req, res) => {
+  if (!req.user || !req.user.avatar) {
+    return handleError(res, undefined, 404, "No user or avatar defined");
+  }
+
+  try {
+    // Send back content type - converts Buffer to an image
+    res.set("Content-Type", "image/png");
+    res.send(req.user.avatar);
+  } catch (e) {
+    return handleError(res, e, 400);
+  }
+});
+
+router.get("/users/:id/avatar", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user || !user.avatar) {
+      return handleError(res, undefined, 404, "No user or avatar defined");
+    }
+
+    // Send back content type - converts Buffer to an image
+    res.set("Content-Type", "image/png");
+    res.send(user.avatar);
+  } catch (e) {
+    return handleError(res, e, 400);
   }
 });
 
