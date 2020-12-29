855be43b53261fcb65421a509e24c00df2edf400
sjoedwards <samuel.j.o.edwards@gmail.com>
2020-12-29 16:10:48 +0000

    Updated connection string from localhost

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGO_CONNECTION_STRING ||
+    "mongodb://127.0.0.1:27017/task-manager-api",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+);
